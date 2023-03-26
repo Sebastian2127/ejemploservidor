1832a6b1c5a0f734ebbf838c68bcc8741c9e9f2b
Sebastian2127 <sebastianverde951@gmail.com>
2023-03-26 12:43:04 -0600

    Código de ejemplo en GitHub

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hola guapisimos, aquí vegeta777
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
